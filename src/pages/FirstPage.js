1169ef5a7b839f0d687e730dad754a026eeb62f1
prince0203 <mc.prince.0203@gmail.com>
2018-02-11 22:25:10 +0900

    Add repositories page

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -3,6 +3,7 @@ import ons from 'onsenui'
 import * as Ons from 'react-onsenui'
 
 import renderToolbar from '../renderToolbar'
+import Repositories from './Repositories'
 
 // 最初に表示されるページ
 export default class FirstPage extends React.Component {
@@ -11,10 +12,15 @@ export default class FirstPage extends React.Component {
 
     // onClickなどで呼ばれたメンバ関数がthisを使えるようにする
     this.showAlert = this.showAlert.bind(this)
+    this.moveToRepositories = this.moveToRepositories.bind(this)
   }
 
   showAlert () {
     ons.notification.alert('Hello world!')
+  }
+
+  moveToRepositories () {
+    this.props.navigator.pushPage({ page: Repositories, key: 'Repositories' })
   }
 
   render () {
@@ -24,6 +30,9 @@ export default class FirstPage extends React.Component {
       >
         <Ons.Button onClick={this.showAlert}>
           Tap me!
+        </Ons.Button>
+        <Ons.Button onClick={this.moveToRepositories}>
+          Repositories
         </Ons.Button>
       </Ons.Page>
     )
---
+++ src/pages/Repositories.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import * as Ons from 'react-onsenui'
+
+import renderToolbar from '../renderToolbar'
+
+// リポジトリ一覧のページ
+export default class Repositories extends React.Component {
+  constructor (props) {
+    super(props)
+
+    // 決め打ちのデータ(後で置き換える)
+    this.repositories = [{
+      name: "LifeGame",
+      description: "A lifegame written with Kotlin + Libgdx"
+    }, {
+      name: "matchland",
+      description: "A libGDX game written in Kotlin. Spread your land to win!"
+    }]
+  }
+
+  // リポジトリのデータを受け取って、ListItemに変換する
+  renderRow (row, index) {
+    return (
+      <Ons.ListItem key={index} tappable>
+        <div className="center">
+          <span className="list-item__title">
+            {row.name}
+          </span>
+          <span className="list-item__subtitle">
+            {row.description}
+          </span>
+        </div>
+      </Ons.ListItem>
+    )
+  }
+
+  render () {
+    return (
+      <Ons.Page
+        renderToolbar={() => renderToolbar(true, 'Repositories', this.props.navigator)}
+      >
+        <Ons.List
+          dataSource={this.repositories}
+          renderRow={this.renderRow}
+        />
+      </Ons.Page>
+    )
+  }
+}
